1543d01f0f0c501b2cb603986c33052ad89ce1e4
Ahm A <asad.gooogle@gmail.com>
2020-10-23 22:08:56 -0400

    Update bulb.ino

--- bulb/bulb.ino
+++ bulb/bulb.ino
@@ -2,8 +2,8 @@
 #include <ESP8266WiFi.h>
 
 // Replace with your network credentials
-const char* ssid     = "FBIHQ_EXT";
-const char* password = "nupsidupsi1204";
+const char* ssid     = "";
+const char* password = "";
 
 // Set web server port number to 80
 WiFiServer server(80);
