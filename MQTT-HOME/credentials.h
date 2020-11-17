eb660b186cd5243799549de91cecd79ec810c8a9
Ahm A <asad.gooogle@gmail.com>
2020-11-16 19:50:15 -0500

    Update credentials.h

--- MQTT-HOME/credentials.h
+++ MQTT-HOME/credentials.h
@@ -1,7 +1,7 @@
 const char* ssid = "WiFiSSID";
 const char* password = "WiFiPASSWORD";
 
-const char* mqtt_server = "10.0.0.76";
+const char* mqtt_server = "10.0.0.76"; // Assigned IP for MQTT Server on a Pi 
 const char* mqtt_user = "USERNAME_FOR_MOSQUITTO_BROKER";
 const char* mqtt_password = "PASSWORD_FOR_MOSQUITTO_BROKER";
 uint16_t mqtt_port = 1883; // default
