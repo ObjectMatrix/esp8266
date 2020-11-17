010f610d4a4b6edadbe2271896176abd717ca577
Ahm A <asad.gooogle@gmail.com>
2020-11-16 19:37:54 -0500

    Create topic.h

---
+++ MQTT-HOME/topic.h
@@ -0,0 +1,21 @@
+
+const char* homeofficeher = "/home/office/her";
+const char* homeofficehis = "/home/office/his";
+
+const char* homeliving = "/home/living";
+const char* homedining = "/home/dining";
+
+const char* homebedroom = "/home/bed/room";
+const char* homekidsroom1 = "/home/kids/room1";
+const char* homekidsroom2 = "/home/kids/room2";
+
+const char* homeguestroom = "/home/guest/room";
+const char* homeentertainment = "/home/entertainment/tv";
+
+const char* homegarage = "/home/garage";
+
+const char* homeswimmingpool = "/home/swimming/pool";
+const char* homeswimmingdoor = "/home/swimming/door";
+
+const char* connecttopic = "/home/connect";
+const char* debugtopic = "/home/debug";
